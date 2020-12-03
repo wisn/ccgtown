a903f074f2f4bed2d27f9d711a74f61f17854048
Wisnu Adi Nurcahyo <wisn.adn@gmail.com>
2020-12-03 15:04:15 +0700

    Preparing deployment for Heroku

--- ccgtown/settings.py
+++ ccgtown/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
